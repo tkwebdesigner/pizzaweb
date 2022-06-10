139fd75e33be7dde7fd14a2f0504543aa142d227
TOFIQUE KHAN <tofique.animation@gmail.com>
2022-06-10 22:30:55 +0530

    Some Imp Change
    Some Imp Change

--- js/main.js
+++ js/main.js
@@ -16,3 +16,31 @@ navBar.forEach(function (a){
         navCollapse.classList.remove("show");
     })
 })
+
+// Change menuicon on Click
+let changeIcon = function(icon){
+    if(icon){
+        icon.classList.toggle("fa-times")
+    }
+    else{
+        icon.classList.remove("fa-times")
+    }
+}
+
+// Using arrow function
+// changeIcon = (icon) => icon.classList.toggle("fa-times");
+
+var btn = $('#btnUp');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
